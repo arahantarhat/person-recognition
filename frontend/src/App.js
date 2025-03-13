a60138d30d1d37130212f15db38ed207bb9597a4
arahantarhat <103532523+arahantarhat@users.noreply.github.com>
2025-03-12 23:08:21 -0400

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>People Recognition App</h1>
+            <p>Frontend is running!</p>
+        </div>
+    );
+}
+
+export default App;
